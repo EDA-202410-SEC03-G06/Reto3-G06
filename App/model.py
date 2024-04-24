c21c9ad981760a5379db2c4f2cfa247a2ed5616b
abenavidesr <a.benavidesr@uniandes.edu.co>
2024-04-23 21:28:24 -0500

    Cambios reto y carga de datos

--- App/model.py
+++ App/model.py
@@ -98,10 +98,62 @@ def add_skills(catalog, skills):
     """
     mp.put(catalog['skills'],skills['id'],skills)
 
-def add_jobs(data_structs,data):
+def add_jobs(data_structs, data):
     occurreddate = data["published_at"]
     oferta_date = datetime.strptime(occurreddate, "%Y-%m-%d")
     entry = om.get(map)
+
+def updateDateIndex(map, job):
+    """
+    Se toma la fecha del crimen y se busca si ya existe en el arbol
+    dicha fecha.  Si es asi, se adiciona a su lista de crimenes
+    y se actualiza el indice de tipos de crimenes.
+
+    Si no se encuentra creado un nodo para esa fecha en el arbol
+    se crea y se actualiza el indice de tipos de crimenes
+    """
+    occurreddate = job['OCCURRED_ON_DATE']
+    jobdate = datetime.strptime(occurreddate, '%Y-%m-%d')
+    entry = om.get(map, jobdate.date())
+    if entry is None:
+        datentry = newDataEntry(job)
+        om.put(map, jobdate.date(), datentry)
+    else:
+        datentry = me.getValue(entry)
+    addDateIndex(datentry, job)
+    return map
+
+def newDataEntry(crime):
+    """
+    Crea una entrada en el indice por fechas, es decir en el arbol
+    binario.
+    """
+    entry = {'dateIndex': None, 'jobs': None}
+    entry['dateIndex'] = m.newMap(numelements=30,
+                                     maptype='PROBING',
+                                     cmpfunction=compareOffenses)
+    entry['jobs'] = lt.newList('SINGLE_LINKED', compareDates)
+    return entry
+
+def addDateIndex(datentry, job):
+    """
+    Actualiza un indice de tipo de crimenes.  Este indice tiene una lista
+    de crimenes y una tabla de hash cuya llave es el tipo de crimen y
+    el valor es una lista con los crimenes de dicho tipo en la fecha que
+    se está consultando (dada por el nodo del arbol)
+    """
+    lst = datentry['jobs']
+    lt.addLast(lst, job)
+    offenseIndex = datentry['dateIndex']
+    offentry = mp.get(offenseIndex, job['OFFENSE_CODE_GROUP'])
+    if (offentry is None):
+        entry = newOffenseEntry(job['published_at'], job)
+        lt.addLast(entry['lstoffenses'], crime)
+        m.put(offenseIndex, job['published_at'], entry)
+    else:
+        entry = me.getValue(offentry)
+        lt.addLast(entry['lstoffenses'], job)
+    return datentry
 
 def add_employment(data_structs,oferta):
     salario = oferta['salary_from']
@@ -210,7 +262,29 @@ def compare(data_1, data_2):
 
 # Funciones de ordenamiento
 
-
+def compareDates(date1, date2):
+    """
+    Compara dos fechas
+    """
+    if (date1 == date2):
+        return 0
+    elif (date1 > date2):
+        return 1
+    else:
+        return -1
+    
+def compareOffenses(offense1, offense2):
+    """
+    Compara dos tipos de crimenes
+    """
+    offense = me.getKey(offense2)
+    if (offense1 == offense):
+        return 0
+    elif (offense1 > offense):
+        return 1
+    else:
+        return -1
+    
 def sort_criteria(id, entry):
     """sortCriteria criterio de ordenamiento para las funciones de ordenamiento
 
